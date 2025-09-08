587b3c277691f2f7fc0176cd77a3dee64f7eaef4
Edun Omatseye <edunomatseye@yahoo.com>
2025-09-08 12:16:41 +0100

    feat: enhance routing and context management in main application
    - Updated main.tsx to include a context with user data for routing.
    - Refactored __root.tsx to use createRootRouteWithContext for improved data loading.
    - Added loader function to retrieve user age from context and display it in the component.
    - Improved code consistency by standardizing import statements and formatting.

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -1,22 +1,42 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanstackDevtools } from "@tanstack/react-devtools";
 
-export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanstackDevtools
-        config={{
-          position: "bottom-left",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
-    </>
-  ),
+export const Route = createRootRouteWithContext<{
+  db: {
+    age: number;
+    name: string;
+    city: string;
+    country: string;
+    isMarried: boolean;
+    hobbies: string[];
+  };
+  query: { getAge: () => number };
+}>()({
+  loader(ctx) {
+    const age = ctx.context.db.age;
+    return {
+      age,
+    };
+  },
+  component: () => {
+    const { age } = Route.useLoaderData();
+    return (
+      <>
+        <div>Age: {age}</div>
+        <Outlet />
+        <TanstackDevtools
+          config={{
+            position: "bottom-left",
+          }}
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </>
+    );
+  },
 });
